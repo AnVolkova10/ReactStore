1d0306872f2cf4d2c61b15094ed244cf3f4d8207
AnVolkova10 <curzi.angela@gmail.com>
2022-01-09 13:13:32 -0300

    added boton arrepentimiento

--- src/js/BuyButton.js
+++ src/js/BuyButton.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/BuyButton.css';
 import { confetti } from './confetti';
 
 function BuyButton() {
+    const [showArrepentimiento, setShowArrepentimiento] = useState(false);
 
     const start = () => {
         setTimeout(function() {
             confetti.start()
         }, 100); 
         // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+        setShowArrepentimiento(true);
     };
 
-    // const stop = () => {
-    //     setTimeout(function() {
-    //         confetti.stop()
-    //     }, 5000); 
+    const stop = () => {
+        setTimeout(function() {
+            confetti.stop()
+        }, 1000); 
+        setShowArrepentimiento(false);
         
-    // };
+    };
 
     
 
     
     return(
+        <section>
         <button className='buy-now'
                 onClick={() => start()}
         >BUY NOW!</button>
+       {showArrepentimiento ?  <button className='me-arrepenti'
+                onClick={() => stop()} 
+        >me arrepentí :c</button> : null}
+        </section>
     );
 }
 
